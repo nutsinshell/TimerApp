7b80107ffaf7b1471f3ec4d77eabd9780eb9e3c8
mizuki <zukif@icloud.com>
2017-08-12 21:15:22 +0900

    Initial Commit

---
+++ TimerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Mizuki on 2017/08/12.
+//  Copyright © 2017年 nutsinshell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
