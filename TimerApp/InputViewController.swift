f63769cc279c40a9c8c1e1c10a6c1e235fd98766
mizuki <zukif@icloud.com>
2017-11-02 21:26:58 +0900

    アプリ名、スプラッシュ画面設定

--- TimerApp/InputViewController.swift
+++ TimerApp/InputViewController.swift
@@ -4,16 +4,16 @@ import SVProgressHUD
 
 //UIデザインのために追加
 extension UIView {
-    @IBInspectable var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            layer.cornerRadius = newValue
-            layer.masksToBounds = newValue > 0
-        }
-    }
-    
+//    @IBInspectable var cornerRadius: CGFloat {
+//        get {
+//            return layer.cornerRadius
+//        }
+//        set {
+//            layer.cornerRadius = newValue
+//            layer.masksToBounds = newValue > 0
+//        }
+//    }
+//    
     @IBInspectable
     var borderWidth: CGFloat {
         get {
